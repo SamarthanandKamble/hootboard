f096d567ffbb1a0fb8c4d96eeacd1c0f5bba0f3f
Samarth1002 <samarthanandkamble1002@gmail.com>
2024-03-28 12:41:19 +0530

    bugFix/handled_the_mixed_content(http/https)

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,7 +1,7 @@
 const apiKey = import.meta.env.VITE_API_KEY;
 
-export const FETCH_CITY_KEY = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=`;
+export const FETCH_CITY_KEY = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=`;
 
-export const FETCH_CITY_INFO = `http://dataservice.accuweather.com/currentconditions/v1/`;
+export const FETCH_CITY_INFO = `https://dataservice.accuweather.com/currentconditions/v1/`;
 
-export const FETCH_CITY_INFO_BY_COORDS = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=`;
+export const FETCH_CITY_INFO_BY_COORDS = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=`;
